f58ec9268429014761d19b1cd975c5adaaaf2879
Anders <techboxnorth@gmail.com>
2024-04-30 11:21:51 +0200

    added login and signup pages with basic styling

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,7 +1,27 @@
-
+import './Login.css'
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <div className="form-container">
+      <h2>Sign Up</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" />
+        </div>
+        <div className="error-msg">
+          <p>Wrong username or password</p>
+        </div>
+        <div className="button-container">
+          <button>Login</button>
+        </div>
+      </form>
+      </div>
+    </div>
   )
 }
 
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,7 +1,39 @@
-
+import './Signup.css'
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='signup'>
+      <div className="form-container">
+      <h2>Sign Up</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="conf-password">Confirm password</label>
+          <input type="password" />
+        </div>
+        <div className="checkbox-group">
+          <input type="checkbox" name="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">I accepts terms and conditions</label>
+        </div>
+        <div className="error-msg">
+          <p>An account with this email already exists</p>
+        </div>
+        <div className="button-container">
+          <button>Sign up</button>
+        </div>
+      </form>
+      </div>
+    </div>
   )
 }
 
